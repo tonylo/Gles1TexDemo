aba944cf169d9bd2785e8ae7e890e59b1e190c22
Tony Lofthouse <tony.lofthouse@gmail.com>
2013-03-09 12:48:18 -0800

    Change the auto hide delay (just playing with git in eclipse really)

--- src/com/example/glestexdemo1/FullscreenActivity.java
+++ src/com/example/glestexdemo1/FullscreenActivity.java
@@ -28,7 +28,7 @@ public class FullscreenActivity extends Activity {
 	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
 	 * user interaction before hiding the system UI.
 	 */
-	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+	private static final int AUTO_HIDE_DELAY_MILLIS = 3001;
 
 	/**
 	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
